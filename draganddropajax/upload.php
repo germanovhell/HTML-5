073e0431dbdaeb6ced1044b609bddd64c19f92dc
Germán Maluenda Alfaro <gmaluenda.alfaro@gmail.com>
2017-05-18 21:17:55 -0400

    upload arreglado para subir imagenes

--- draganddropajax/upload.php
+++ draganddropajax/upload.php
@@ -1,11 +1,14 @@
 <?php
 
-$nombreArchivo = $_POST["user_file"]["name"];
-$tipoArchiv = $_POST["user_file"]["type"];
-$tempArchivo = $_POST["user_file"]["tmp_name"];
-if(move_uploaded_file($tempArchivo,"uploads/".$nombreArchivo)){
-    echo "uploads/".$nombreArchivo;
-}else{
-    print_r($_FILES);
+$nombreArchivo = $_FILES['user_file']['name'];
+$tipoArchivo = $_FILES['user_file']['type'];
+$tempArchivo = $_FILES['user_file']['tmp_name'];
+if(move_uploaded_file($tempArchivo,"uploads/".$nombreArchivo))
+{
+	echo "uploads/".$nombreArchivo;
 }
-?>
+else
+{
+	print_r($_FILES);
+}
+
